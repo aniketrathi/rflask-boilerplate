55ead66077e4b868c3cb3ac396d72ad6cfb5df73
aniketrathi <aniketrathi897@gmail.com>
2024-08-06 22:47:56 +0530

    improved indentation

--- src/apps/backend/modules/vendor_account/errors.py
+++ src/apps/backend/modules/vendor_account/errors.py
@@ -7,6 +7,7 @@ class VendorAccountWithSameNameAndAccountExistsError(AppError):
         message = f"A vendor account with the name {vendor_account_name} has already been created. Please choose a different name."
         super().__init__(code=VendorAccountErrorCode.NAME_ALREADY_EXISTS, https_status_code=409, message=message)
 
+
 class VendorAccountNotFoundError(AppError):
     def __init__(self, vendor_account_id: str) -> None:
         message = f"Vendor account with id {vendor_account_id} not found. Please verify the id and try again."
