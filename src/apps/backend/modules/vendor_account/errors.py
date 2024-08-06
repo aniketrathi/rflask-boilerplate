800dc0d4a298539a49d63c8e6873980ce6445323
aniketrathi <aniketrathi897@gmail.com>
2024-08-06 16:02:34 +0530

    removed unwanted code

--- src/apps/backend/modules/vendor_account/errors.py
+++ src/apps/backend/modules/vendor_account/errors.py
@@ -4,6 +4,5 @@ from modules.vendor_account.types import VendorAccountErrorCode
 
 class VendorAccountWithSameNameAndAccountExistsError(AppError):
     def __init__(self, vendor_account_name: str) -> None:
-        print(vendor_account_name);
         message = f"A vendor account with the name {vendor_account_name} has already been created. Please choose a different name."
         super().__init__(code=VendorAccountErrorCode.NAME_ALREADY_EXISTS, https_status_code=409, message=message)
